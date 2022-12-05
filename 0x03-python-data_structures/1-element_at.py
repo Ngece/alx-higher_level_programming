c8f6a5e8eadec2e3fea346426af7aaa148bc5ce4
Ngece <ngecemlamli@gmail.com>
2022-12-05 05:19:08 -0800

    committing elementat fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 def element_at(my_list, idx):
-    """Retrive an element from a list."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return None
-    return (my_list[idx])
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
