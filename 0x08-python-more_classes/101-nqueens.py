77caa95c5fef979f35cf3f7379ff0f67878b3266
Ngece <ngecemlamli@gmail.com>
2023-01-03 18:55:14 -0800

    commmitting nqueens fixed

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -10,9 +10,9 @@ This program solves the N queens problem.
         If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
     The program should print every possible solution to the problem
         One solution to the problem
-        """
+"""
 
-        import sys
+import sys
 
 
 def init_board(n):
