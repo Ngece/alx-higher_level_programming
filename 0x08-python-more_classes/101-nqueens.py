8b34f76201c1765615fb2169e77ed10dd6535d30
Ngece <ngecemlamli@gmail.com>
2023-01-03 18:39:31 -0800

    committing nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -9,10 +9,10 @@ This program solves the N queens problem.
         If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
         If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
     The program should print every possible solution to the problem
-        One solution per line
-"""
+        One solution to the problem
+        """
 
-import sys
+        import sys
 
 
 def init_board(n):
