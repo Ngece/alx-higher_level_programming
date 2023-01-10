0d9e0139b4b9f341eb50e7215bb96bb02daf541c
Ngece <ngecemlamli@gmail.com>
2023-01-09 18:41:32 -0800

    committing is-same-class changed again

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,4 +2,14 @@
 """Checks object class"""
 
 def is_same_class(obj, a_class):
-    return True if type(obj) is a_class else False 
+    """Check if an object is exactly an instance of a given class.
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
