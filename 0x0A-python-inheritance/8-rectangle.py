4e7ebca08c6139e084637d5c635a91b8ca7b55c5
Ngece <ngecemlamli@gmail.com>
2023-01-09 18:51:09 -0800

    committing 8-rectangle changed

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Inherits BaseGeometry class"""
 
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
-    
+    """Represent a rectangle using BaseGeometry."""
+
     def __init__(self, width, height):
         """Intialize a new Rectangle.
         Args:
