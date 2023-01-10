936866f56bf7a520e22606616fe5b6f237b0ef20
Ngece <ngecemlamli@gmail.com>
2023-01-09 19:02:41 -0800

    committing my-int with more comments

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
+"""
+Defines the class MyInt
+"""
+
+
 class MyInt(int):
-    
-    def __eq__(self, other)
-        return super().__ne__(other)
+    """rebel version of an int"""
+    def __eq__(self, other):
+        """returns the opposite of equal(eq)"""
+        return int(self) != other
 
     def __ne__(self, other):
-        return super().__eq__(other)
+        """returns the opposite of not equal(ne)"""
+        return int(self) == other
