51c33d92a0d53e744a42e5f5d3fbc3069af60c85
Ngece <ngecemlamli@gmail.com>
2023-01-10 20:00:09 -0800

    coomiitting 10-student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,6 +2,7 @@
 
 """Defines a class Student."""
 
+
 class Student:
     """Represent a student."""
 
@@ -26,4 +27,4 @@ class Student:
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.
+        return self.__dict__
