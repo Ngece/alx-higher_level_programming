ce3ea649affc3ac1d18c53d71ac8271cee5f666d
Ngece <ngecemlamli@gmail.com>
2023-03-13 12:12:05 -0700

    committing concat again

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,4 @@
 
 if (process.argv[3]) {
 	console.log(process.argv[2] + ' is ' + process.argv[3]);}
-	else if (process.argv[2] {
-		console.log(process.argv[2] + ' is undefined');}
-		else {
-			console.log('undefined is undefined');}
+
