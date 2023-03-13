e8a15e853e25f0066d5e2a7fecfed3ca38dab33f
Ngece <ngecemlamli@gmail.com>
2023-03-13 13:32:57 -0700

    committing the whole project

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length === 0){
-	console.log('No argument');}
-	else if (process.argv.length === 1) {
-		console.log('Argument found');}
-	else (process.argv.length > 1) {
-		console.log('Arguments found');}
-
+if (process.argv.length > 3) {
+  console.log('Arguments found');} 
+else if (process.argv.length === 3) {
+  console.log('Argument found');} 
+else {
+  console.log('No argument');}
 
