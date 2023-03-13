b2abf5f48048297fe6955563ee046dcd1672c91f
Ngece <ngecemlamli@gmail.com>
2023-03-13 13:39:16 -0700

    committing the whole project

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (size) {
   for (let i = 0; i < size; ++i) {
     let p = 0;
 
-    for (; p < size; ++j) {
+    for (; p < size; ++p) {
       process.stdout.write('X');}
 
     if (p === size) {
