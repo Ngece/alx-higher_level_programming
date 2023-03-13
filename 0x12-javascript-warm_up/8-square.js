86f6a23b419b1025e85d2fa43b0b7c2dc2ae5a50
Ngece <ngecemlamli@gmail.com>
2023-03-13 12:47:34 -0700

    committing square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (size) {
+  for (let i = 0; i < size; ++i) {
+    let p = 0;
+
+    for (; p < size; ++j) {
+      process.stdout.write('X');}
+
+    if (p === size) {
+      console.log('');}}
+} else {
+  console.log('Missing size');}
