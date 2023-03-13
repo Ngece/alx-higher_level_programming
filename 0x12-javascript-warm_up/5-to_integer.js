cd8417c6fe362395fd055fbad51708615833882e
Ngece <ngecemlamli@gmail.com>
2023-03-13 12:29:57 -0700

    comitting

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv[2] => 0) {
-	console.log('My number: ' + parseint(process.argv[2]));}
-else {
-	console.log('Not a number');}
+const arg = parseInt(process.argv[2]);
+
+if (arg) {
+  console.log('My number: ' + arg);
+} else {
+  console.log('Not a number');
+}
