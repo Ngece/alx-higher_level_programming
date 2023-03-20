38f8cef35b800cd7be47e113d8b0da1fcc30195a
Ngece <ngecemlamli@gmail.com>
2023-03-20 14:40:33 -0700

    committing the states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Lists all states from the database hbtn_0e_0_usa running on local host.
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    access database and print the states
+    """
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY ID")
+    [print(state) for state in cur.fetchall()]
+
