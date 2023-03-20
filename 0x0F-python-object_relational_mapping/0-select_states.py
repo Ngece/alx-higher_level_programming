45bafd2ea8b1710dd26bb4be806f504b794cb413
Ngece <ngecemlamli@gmail.com>
2023-03-20 15:12:19 -0700

    committing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa running on local host.
+""" 
+Lists all states from the database hbtn_0e_0_usa running on local host.
+"""
 
 import MySQLdb
 from sys import argv
