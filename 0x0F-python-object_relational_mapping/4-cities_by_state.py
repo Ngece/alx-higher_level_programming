1fb3ef859441d09ff38729722b1cfb26b80a5184
Ngece <ngecemlamli@gmail.com>
2023-03-20 16:29:01 -0700

    committing

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 
+Lists all states from the database hbtn_0e_0_usa running on local host.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access database and print state that matches the argument given but safe from an sql injection.
+    """
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    r = cursor.fetchall()
+
+    for row in r:
+        print(row)
