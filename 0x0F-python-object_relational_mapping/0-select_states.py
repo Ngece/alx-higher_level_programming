890bb56c3820cae4afe4b58ad5476b380eda5f19
Ngece <ngecemlamli@gmail.com>
2023-03-20 14:56:42 -0700

    committing select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,5 +12,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY ID")
-    [print(state) for state in cur.fetchall()]
+    outp = cur.fetchall
+
+    for i in outp:
+        print(i)
 
