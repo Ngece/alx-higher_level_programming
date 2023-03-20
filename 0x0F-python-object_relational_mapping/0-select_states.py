dd1ebfaa2970efb4aae5e6f4e58d78eedb5fffd2
Ngece <ngecemlamli@gmail.com>
2023-03-20 15:56:19 -0700

    committing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     r = cur.fetchall()
 
    for row in r:
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 
+Lists all states from the database hbtn_0e_0_usa running on local host.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access database and print states starting with the letter N.
+    """
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4]))
+    r = cur.fetchall()
+
+    for row in r:
+        print(row)
