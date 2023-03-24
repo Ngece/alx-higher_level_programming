c7892c546d3ce3cbf87ef4f7b82d0ff8ae7df47e
Ngece <ngecemlamli@gmail.com>
2023-03-24 14:09:04 -0700

    committing model state fetch first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,29 +4,20 @@
 Prints the first State object from the database hbtn_0e_6_usa
 """
 
-from sys import argv
-from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from model_state import State
 
 if __name__ == "__main__":
-
-    # make engine for database
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # query first python instance in database
-    first_instance = session.query(State).order_by(State.id).first()
-    if first_instance:
-        print("{:d}: {:s}".format(first_instance.id, first_instance.name))
-    else:
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
-
-    session.close()
-
+    else:
+        print("{}: {}".format(state.id, state.name))
