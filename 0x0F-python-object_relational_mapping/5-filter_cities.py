a7737fbebf245e6c1ca6be0e7162319b9ef83de2
Ngece <ngecemlamli@gmail.com>
2023-03-24 13:51:52 -0700

    committing filter-cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,19 +3,11 @@
 Lists all states from the database hbtn_0e_0_usa running on local host.
 """
 
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access database and print all cities based on state name which 
-    provided as an argument.
-    """
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-
-    cursor = db.cursor()
-    cursor.execute("SELECT *cities.id, cities.name FROM cities JOIN states ON states.id = cities.state_id WHERE states.name LIKE BINARY %(state_name)s ORDER BY cities.id ASC", {'state_name': argv[4]})
-    r = cursor.fetchall()
-
-    for row in r:
-        print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` INNER JOIN `states` as `s` ON `c`.`state_id` = 's'.id ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
