ea9ca088b0219c2c57a28b93197c32c4057fc5bb
Ngece <ngecemlamli@gmail.com>
2023-03-30 13:35:23 -0700

    committing unfinished project

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,5 @@
-#!bin/bash
+#!/bin/bash
+
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
 
-curl -sL -X PUT -H "Origin: " -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -i -X 0.0.0.0:5000/catch_me
